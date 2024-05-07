33886d94ec1826fdbb7652a9f48c7237d09abdb1
ilhammdd <165543551+ilhammmdd@users.noreply.github.com>
2024-05-07 23:12:53 +0700

    commit 1

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1 :class="{ active: isActive }" :style="{ fontSize: fontSize + 'px' }">{{ title }}</h1>
+    <input type="text" v-model="inputValue" @input="handleInput" placeholder="Masukkan teks disini...">
+    <p>Input value: {{ inputValue }}</p>
+    <button @click="increaseFontSize">Besarkan Judul</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Hi Kawan Teknikku',
+      isActive: false,
+      fontSize: 24,
+      inputValue: ''
+    }
+  },
+  methods: {
+    handleInput() {
+      this.isActive = !this.isActive;
+    },
+    increaseFontSize() {
+      this.fontSize += 1;
+    },
+  
+  }
+}
+</script>
+
+<style>
+.active {
+  color: red;
+}
+</style>
